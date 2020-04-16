7a150f20f56ea263bf41a5de6457301030078c87
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-04-16 22:43:36 +0530

    Timer circle transition with JS

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
 const timerLabel = document.getElementById('timer-label');
 const remainingTimerCircle = document.getElementById('remainingCircle');
 
-const TIME_LIMIT = 90;
+const TIME_LIMIT = 20;
+const TOTAL_PATH_LENGTH = 251;/*Math.floor(2*Math.PI*40);*/
+
 let passedTime = 0;
 let remainingTime = TIME_LIMIT;
 let timerInterval = null;
 
-startTimer();
+startTimerMethod2();
 
-function startTimer(){
+
+function startTimerMethod2(){
+
+    /* required to prevent initial sudden transition of 1s */
+    remainingTimerCircle.setAttribute("stroke-dasharray","251 251");
+
+    /* show the initial time limit in the clock circle before the countdown starts*/
     setUpTimerLabel(remainingTime);
+    
+    /* set up the countdown which is asynchronous code */
     timerInterval = setInterval(()=>{
-        if(remainingTime===TIME_LIMIT)
-            animateRemainingCircle();
+        
+        /* countdown */
         passedTime += 1;
         remainingTime = TIME_LIMIT - passedTime;
         setUpTimerLabel(remainingTime);
+
+        redrawTimerCircle();
+
+        /* stop the timer */
+        if(remainingTime===0){
+            clearInterval(timerInterval);
+            /* required to achieve the effect of forward fill as in css animation */
+            remainingTimerCircle.setAttribute("stroke-dasharray","0 251");
+        }
+    },1000)
+}
+
+function redrawTimerCircle(){
+    const remainingTimerCircleLength = calculateRemainingTimeFraction()*TOTAL_PATH_LENGTH;
+    remainingTimerCircle.setAttribute("stroke-dasharray",`${remainingTimerCircleLength} ${TOTAL_PATH_LENGTH}`);
+}
+
+function calculateRemainingTimeFraction(){
+    /* extra subtraction factor to overcome the lag between text and circle when trying to achieve a smooth transition effect */
+    return (remainingTime/TIME_LIMIT) - (1 / TIME_LIMIT) * (1 - remainingTime/TIME_LIMIT);
+}
+
+function startTimerMethod1(){
+
+    /* show the initial time limit in the clock circle before the countdown starts*/
+    setUpTimerLabel(remainingTime);
+
+    /* set up the countdown which is asynchronous code */
+    timerInterval = setInterval(()=>{
+        /* since the countdown starts asynchronously start animating once this code executes */
+        if(remainingTime===TIME_LIMIT)
+            animateRemainingCircle();
+        /* countdown */
+        passedTime += 1;
+        remainingTime = TIME_LIMIT - passedTime;
+        setUpTimerLabel(remainingTime);
+
+        /* stop the timer */
         if(remainingTime===0)
             clearInterval(timerInterval);
     },1000)
